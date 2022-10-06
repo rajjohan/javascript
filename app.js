aabf247eb907d62ed50890f28917788189d7bd62
rajjohan <rm8007716@email.com>
2022-10-06 04:05:13 -0700

    data type

---
+++ app.js
@@ -0,0 +1,403 @@
+// data types
+
+
+String
+
+let name = 'rajuuuu'
+console.log( name);
+
+let num = "hassan"
+console.log( num);
+
+let g = 'ali'
+console.log( g);
+
+let h = 'fayyaz'
+console.log(h);
+
+Number
+
+let number = 123;
+console.log(number);
+
+let nubr = 123;
+console.log(nubr);
+
+let gip = 320387;
+console.log(gip);
+
+
+
+
+undefined
+
+let a;
+console.log(a);
+
+let khano;
+console.log(khano);
+
+let trust = undefined;
+console.log(trust);
+
+let b = undefined;
+console.log(b);
+
+
+
+null
+
+let c = null
+console.log(c);
+
+let l = null
+console.log(l);
+
+let optics = null
+console.log(optics);
+
+let m = null
+
+let n = null
+
+Boolean
+
+var e = true
+console.log(e);
+
+let o = true
+console.log(o);
+
+var f = false 
+console.log(f);
+
+// NON PREMITIVE
+
+// 1: Object
+
+// let Object = {
+//     name: 'ali',
+//     age: 22,
+// }
+// console.log(Object)
+
+// let hgdc = {
+//     name: 'aly khan',
+//     age: 20,
+//     addrees: 'gilgit',
+// }
+// console.log(hgdc)
+
+// 2: Array
+// let Arrayw = ['khano', 22, 'danyore', 'baig market']
+// console.log(Arrayw)
+
+// let jhu = [
+//     ['hassan', 22, 'gilgitc', 'danyoresdc'],
+//     ['hassandc', 12, 'gilgitxs', 'danyored'],
+//     ['hassand', 52, 'gilgitdc', 'danyorew']
+// ]
+// console.log(jhu)
+
+
+// JAVASCRIPT UPPERCASE
+
+// let numw = 'The main difference between String Literal and String Object is that String Literal is a String created using double quotes while String Object is a String created using the new() operator.'
+// console.log(numw)
+// let num2 = numw.toUpperCase()
+// console.log(num2)
+
+// JAVASCRIPT LOWERCASE
+// let num3 = numw.toLowerCase()
+// console.log(num3)
+
+// MODIFY Array AND DELET Array]
+
+// let raju = ['raju', 19, 'askurdas']
+// console.log(raju)
+// raju [0] = 'raja mehdi'
+// console.log(raju)
+
+// let hsdg = delete raju [1]
+// console.log(hsdg)
+
+// console.log(raju)
+
+
+// ARRAY METHODS
+
+// Pop()
+// let studnt = ['gdwg', 74, 'fgua']
+// console.log(studnt)
+
+// studnt.pop()
+// console.log(studnt)
+
+// PUSH()
+// studnt.push('ugfuy', 34,)
+// console.log(studnt)
+
+// SHIFT()
+// studnt.shift()
+// console.log(studnt)
+
+
+// UNSHIFT()
+// studnt.unshift('gduysagf',653,'efggfe')
+// console.log(studnt)
+
+
+
+
+// =====================ARRAY METHODS=======================
+
+// CONCATINATION
+
+// let array1 = [ 'khano', 'fayoo', 'raju']
+// console.log(array1)
+
+// let array2 = ['khano', 'fayoo','raju']
+// console.log(array2)
+
+// let output = array1.concat(array2)
+// console.log(output)
+
+
+// sorting array
+
+// let something1 = ['sdg','sdafa','efawe','eawf']
+// console.log(something1)
+
+// let some = something1.sort()
+// console.log(some)
+
+
+// ALERT
+
+// alert('teri')
+
+
+
+// CONFRIM
+// confirm('gand daga kiya')
+
+// let asdh = {
+//     name: 'size',
+//     age: 22/23/24,
+// }
+
+// confirm(asdh.name)
+
+// confirm(asdh.age)
+
+
+// PROMPT
+
+// let jhd = prompt('enter size')
+// console.log(jhd)
+
+// OPERATORS
+
+// EQUAL TO
+
+// let asad = 5
+// console.log(asad==6)
+
+
+// let sjdh = 7
+// console.log(sjdh==7)
+// console.log(sjdh!==7)
+// console.log(sjdh===7)
+
+
+// LOGICAL OPERATORS
+
+// AND
+
+// let sbdh = 45
+// let hud = 34
+
+// console.log(sbdh<44&&hud>35)
+
+
+// OR
+
+// let y = 67
+// let z = 65
+
+// console.log(y==70 || z==60)
+// console.log(y==67 || z==7)
+// console.log(y==70 || z==65)
+// console.log(y==67 || z==65)
+
+// NOT
+
+// let u = 6
+// let t = 9
+//  console.log(!u==t)
+//  console.log(!u>t)
+//  console.log(!u<t)
+
+
+//  ARRAY
+// let color = ["Blue ", "Orange", "Violet", "Green",  "Indigo", "Yellow ", "Red",];
+// console.log(color)
+
+// let choice1 = color[0]
+// console.log("choice one",choice1)
+// let choice2 = color[3]
+// console.log("choice two",choice2)
+// let choice3 = color[6]
+// console.log("choice three",choice3)
+
+// STATMENTS
+
+// if statment
+// else statment
+// else if statments
+
+// let marks = prompt('enter your marks')
+
+// if(marks <= 100 && marks >= 80){
+//     alert('Grade A+')
+// }
+
+// else if(marks <= 79 && marks >= 70){
+//     alert('Grade A')
+// }
+// else if(marks <= 69 && marks >= 60){
+//     alert('Grade B')
+// }
+// else if(marks <= 59 && marks >= 50){
+//     alert('Grade C')
+// }
+// else if(marks <= 49 && marks >= 40){
+//     alert('Grade D')
+// }
+// else if(marks <= 39 && marks >= 1){
+//     alert('ultra pro max fail')
+
+// }
+// else{
+//     alert('Enter A Valid Marks')
+// }
+
+// let weeks = prompt("please enter your day name ")
+// if (weeks <= 1 && weeks >= 1 ){
+//   alert("Monday")
+// }
+// else if( weeks <= 2 && weeks >= 2){
+//   alert("tuesday")
+// }
+// else if( weeks <= 3 && weeks >= 3){
+//   alert("wednesday")
+// }
+// else if( weeks <= 4 && weeks >= 4){
+//   alert("thursday")
+// }
+// else if( weeks <= 5 && weeks >= 5){
+//   alert("friday")
+// }
+// else if( weeks <= 6 && weeks >= 6){
+//   alert("saturday")
+// }
+// else if( weeks <= 7 && weeks >= 7){
+//   alert( "sunday ")
+// }else{
+//   alert("pleas enter a valid day ")
+// }
+
+// let week = prompt ('Enter a day number')
+
+// switch ( week) {
+//     case '1','monday':
+//         alert('monday')
+//         break;
+
+//         case '2','tuesday':
+//         alert('tuesday')
+//         break;
+
+//         case '3','wednesday':
+//         alert('wednesday')
+//         break;
+
+//         case '4','thursday':
+//         alert('thursday')
+//         break;
+
+//         case '5','friday':
+//         alert('friday')
+//         break;
+
+//         case '6','sutarday':
+//         alert('sutarday')
+//         break;
+
+//         case '7','sunday':
+//         alert('sunday')
+//         break;
+
+//     default:
+//         alert('enter a valid number')
+//         break;
+// }
+
+
+
+
+// let marks = prompt ('Enter your marks')
+
+// switch ( marks <= 99 && marks >= 90) {
+//     case '90 <= 100':
+//         alert('A+')
+//         break;
+
+//         case '2','tuesday':
+//         alert('A')
+//         break;
+
+//         case '3','wednesday':
+//         alert('B')
+//         break;
+
+//         case '4','thursday':
+//         alert('C')
+//         break;
+
+//         case '5','friday':
+//         alert('D')
+//         break;
+
+//     default:
+//         alert('enter a valid number')
+//         break;
+// }
+
+
+// let b = 1
+// while(b <= 10){
+//     document.write( b +'    raju <br>' )
+//     b = b + 1
+// }
+
+ 
+// let ca = 1
+
+// do{
+//     document.write( ca +'hassana <br>')
+
+//     ca = ca + 1
+
+// }while(ca <= 10) 
+
+// for(let bs = 1; bs <= 10; bs++){
+//     document.write('nothing <br>')
+// }
+
+
+
+
+
+
+
